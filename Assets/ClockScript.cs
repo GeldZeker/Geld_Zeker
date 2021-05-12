97b5e4e33fb4fd011fff15f17394b7f4a2f493ce
Timo Timmermans <timo_timmermans@hotmail.com>
2021-05-12 16:13:19 +0200

    Initial Scene setup

--- Assets/ClockScript.cs
+++ Assets/ClockScript.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 
 public class ClockScript : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject secondHand;
+    [SerializeField]
+    private GameObject minuteHand;
+    [SerializeField]
+    private GameObject hourHand;
+    [SerializeField]
+    private bool timelapseMode = false;
 
-    public GameObject secondHand;
-    public GameObject minuteHand;
-    public GameObject hourHand;
+    private bool timelapseGoing;
+
+    private bool animationInitiate = true;
+
     private string oldSeconds = null;
     
 
@@ -30,14 +39,59 @@ public class ClockScript : MonoBehaviour
         int minutesInt = int.Parse(inGameTime.ToString("mm"));
         int hoursInt = int.Parse(inGameTime.ToString("hh"));
         float hourDistance = (float)(minutesInt) / 60f;
+        int time = 1;
 
-        iTween.RotateTo(secondHand, iTween.Hash("z", secondsInt * 6 * -1, "time", 1, "easetype", "easeOutQuint"));
-        iTween.RotateTo(minuteHand, iTween.Hash("z", minutesInt * 6 * -1, "time", 1, "easetype", "easeOutElastic"));
+        if (timelapseMode)
+        {
+            if (timelapseGoing == false && animationInitiate == true)
+            {
+                timelapseGoing = true;
+                StartCoroutine(Timelapse());
+            }
+            if (animationInitiate == true)
+            {
+                iTween.RotateTo(minuteHand, iTween.Hash("z", -3600, "time", 10, "easetype", "linear"));
+                iTween.RotateTo(hourHand, iTween.Hash("z", -3600, "time", 100, "easetype", "linear"));
+            }
+        } else
+        {
+            iTween.RotateTo(secondHand, iTween.Hash("z", secondsInt * 6 * -1, "time", time, "easetype", "easeOutQuint"));
+            iTween.RotateTo(minuteHand, iTween.Hash("z", minutesInt * 6 * -1, "time", time, "easetype", "easeOutElastic"));
 
-        if (hoursInt > 12) hoursInt = hoursInt - 12;
+            if (hoursInt > 12) hoursInt = hoursInt - 12;
 
-        if (minutesInt == 0 || minutesInt == 12 || minutesInt == 24 || minutesInt == 36 || minutesInt == 48) {
-            iTween.RotateTo(hourHand, iTween.Hash("z", (hoursInt + hourDistance) * 30 * -1, "time", 1, "easetype", "easeOutQuint"));
+            if (minutesInt == 0 || minutesInt == 12 || minutesInt == 24 || minutesInt == 36 || minutesInt == 48)
+            {
+                iTween.RotateTo(hourHand, iTween.Hash("z", (hoursInt + hourDistance) * 30 * -1, "time", 1, "easetype", "easeOutQuint"));
+            }
         }
+    }
+
+    private IEnumerator Timelapse()
+    {
+        float timer = 0.0f;
+
+        while (timelapseGoing)
+        {
+            timer += Time.deltaTime;
+            int seconds = (int)(timer % 60);
+
+            Debug.Log(seconds);
+
+            if (seconds == 10)
+            {
+                iTween.Stop(minuteHand);
+                iTween.Stop(hourHand);
+                StopTimer();
+            }
+
+            yield return null;
+        }
+    }
+
+    private void StopTimer()
+    {
+        timelapseGoing = false;
+        animationInitiate = false;
     }
 }
---
+++ Assets/VolunteerWorkProgressBar.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolunteerWorkProgressBar : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject Arrow1 = null;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

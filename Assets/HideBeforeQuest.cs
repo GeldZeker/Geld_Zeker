95185a9bcc30ec04582d2830a8dfb622711e6b03
Timo Timmermans <timo_timmermans@hotmail.com>
2021-05-21 17:37:45 +0200

    Button Redo
    Image update

---
+++ Assets/HideBeforeQuest.cs
@@ -0,0 +1,19 @@
+﻿using BWolf.Utilities.PlayerProgression.Quests;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideBeforeQuest : MonoBehaviour
+{
+    [SerializeField]
+    private Quest quest = null;
+
+    [SerializeField]
+    private GameObject toHideGameObject = null;
+
+    private void Update()
+    {
+        if (quest.IsActive) toHideGameObject.SetActive(true);
+        if (!quest.IsActive && !quest.IsCompleted) toHideGameObject.SetActive(false);
+    }
+}
